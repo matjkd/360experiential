9104d4e9562c123ba4ea73a1107d756931196930
Mat Sadler <mat@redstudio.co.uk>
2013-01-10 12:59:28 +0000

    updating clouds

--- js/smoke_edgeActions.js
+++ js/smoke_edgeActions.js
@@ -1,204 +1,122 @@
-/***********************
-* Adobe Edge Composition Actions
-*
-* Edit this file with caution, being careful to preserve 
-* function signatures and comments starting with 'Edge' to maintain the 
-* ability to interact with these actions from within Adobe Edge
-*
-***********************/
-(function($, Edge, compId){
-var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
 
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName) {
-
-
-
-
-
-
-})("stage");
+(function(symbolName){})("stage");
 //Edge symbol end:'stage'
 
 //=========================================================
 
 //Edge symbol: 'smoke-1'
-(function(symbolName) {
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 28500, function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play("start1");
-// insert code here
-});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",28500,function(sym,e){sym.play("start1");});
 //Edge binding end
-
-
-
-
-
-
 })("smoke-1");
 //Edge symbol end:'smoke-1'
 
 //=========================================================
 
 //Edge symbol: 'viral'
-(function(symbolName) {
-
-
-})("viral");
-//Edge symbol end:'viral'
+(function(symbolName){})("ambientmedia");
+//Edge symbol end:'ambientmedia'
 
 //=========================================================
 
 //Edge symbol: 'ambient'
-(function(symbolName) {
-
-
-Symbol.bindElementAction(compId, symbolName, "${_ambient}", "click", function(sym, e) {
-
-});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
 //Edge binding end
-
-
-
-
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 0, function(sym, e) {
-sym.stop();
-});
+Symbol.bindElementAction(compId,symbolName,"${_cloudhover}","mouseover",function(sym,e){});
 //Edge binding end
-
-Symbol.bindElementAction(compId, symbolName, "${_cloudhover}", "mouseover", function(sym, e) {
-
-});
+Symbol.bindElementAction(compId,symbolName,"${_cloudhover}","mouseout",function(sym,e){});
 //Edge binding end
-
-Symbol.bindElementAction(compId, symbolName, "${_cloudhover}", "mouseout", function(sym, e) {
-
-// insert code for mouse leave here
-});
-//Edge binding end
-
-Symbol.bindElementAction(compId, symbolName, "${_ambient}", "mouseover", function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play(1000);
-// insert code for mouse enter here
-});
-//Edge binding end
-
-
-})("ambient");
-//Edge symbol end:'ambient'
+})("guerillamarketing");
+//Edge symbol end:'guerillamarketing'
 
 //=========================================================
 
 //Edge symbol: 'guerilla'
-(function(symbolName) {
-
-
-})("guerilla");
-//Edge symbol end:'guerilla'
+(function(symbolName){})("viralmarketing");
+//Edge symbol end:'viralmarketing'
 
 //=========================================================
 
 //Edge symbol: 'ambientmove'
-(function(symbolName) {
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 24960, function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play("start");
-// insert code here
-});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play("start");});
 //Edge binding end
-
-
-Symbol.bindElementAction(compId, symbolName, "${_ambient3}", "mouseover", function(sym, e) {
-// insert code for mouse enter here
-hoverAmbient();
-});
+Symbol.bindElementAction(compId,symbolName,"${_ambient3}","mouseover",function(sym,e){hoverAmbient();});
 //Edge binding end
-
-Symbol.bindElementAction(compId, symbolName, "${_ambient3}", "mouseout", function(sym, e) {
-// insert code for mouse leave here
-unhoverAmbient();
-});
+Symbol.bindElementAction(compId,symbolName,"${_ambient3}","mouseout",function(sym,e){unhoverAmbient();});
 //Edge binding end
-
-Symbol.bindElementAction(compId, symbolName, "${_ambient3}", "click", function(sym, e) {
-// insert code for mouse leave here
-//CloudpopUp();
-});
-//Edge binding end
-
-
-})("ambientmove");
-//Edge symbol end:'ambientmove'
+})("guerrillaMove");
+//Edge symbol end:'guerrillaMove'
 
 //=========================================================
 
 //Edge symbol: 'ambientmove Copy 1'
-(function(symbolName) {
-
-
-})("ambientmove Copy 1");
+(function(symbolName){})("ambientmove Copy 1");
 //Edge symbol end:'ambientmove Copy 1'
 
 //=========================================================
 
 //Edge symbol: 'guerillamove'
-(function(symbolName) {
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 25000, function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play("start");
-// insert code here
-});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play("start");});
 //Edge binding end
-
-
-
-})("guerillamove");
-//Edge symbol end:'guerillamove'
+})("viralMove");
+//Edge symbol end:'viralMove'
 
 //=========================================================
 
 //Edge symbol: 'viralmove'
-(function(symbolName) {
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 25000, function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play("start");
-// insert code here
-});
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play("start");});
 //Edge binding end
-
-Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 37492, function(sym, e) {
-// play the timeline from the given position (ms or label)
-sym.play("start");
-// insert code here
-});
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",37492,function(sym,e){sym.play("start");});
 //Edge binding end
-
-
-
-})("viralmove");
-//Edge symbol end:'viralmove'
+})("ambientMove");
+//Edge symbol end:'ambientMove'
 
 //=========================================================
 
 //Edge symbol: 'ambient2'
-(function(symbolName) {
-
-})("ambient2");
+(function(symbolName){})("ambient2");
 //Edge symbol end:'ambient2'
 
 //=========================================================
 
-
-
+//Edge symbol: 'cloudhover'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_smoke-13}","mouseover",function(sym,e){hoverAmbient();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_smoke-13}","click",function(sym,e){CloudpopUp();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_smoke-13}","mouseout",function(sym,e){mouseoutAmbient();});
+//Edge binding end
+})("cloudhover");
+//Edge symbol end:'cloudhover'
 
 //=========================================================
 
 //Edge symbol: 'smoke'
-(function(symbolName) {
-
-})("smoke");
+(function(symbolName){})("smoke");
 //Edge symbol end:'smoke'
 
-})(jQuery, AdobeEdge, "smokelinks");
+//=========================================================
+
+//Edge symbol: 'ambientmedia_1'
+(function(symbolName){})("experientialMarketingMove");
+//Edge symbol end:'experientialMarketingMove'
+
+//=========================================================
+
+//Edge symbol: 'experirntialMove'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",37500,function(sym,e){sym.play("start");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play("start");});
+//Edge binding end
+})("experirntial");
+//Edge symbol end:'experirntial'
+
+//=========================================================
+
+//Edge symbol: 'experientialmove'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30000,function(sym,e){sym.play("start");});
+//Edge binding end
+})("experientialmove");
+//Edge symbol end:'experientialmove'
+})(jQuery,AdobeEdge,"smokelinks");
